66482617e4de6a8de79235b77f9ccb18c5b517af
Donald Newholm <newholm1@hotmail.com>
2019-12-28 20:14:37 +0000

    Update feature test with @capacity variable

--- features/feature.rb
+++ features/feature.rb
@@ -3,5 +3,5 @@ plane = Plane.new
 airport = Airport.new
 airport.land(plane)
 airport.launch(plane)
-1000.times { airport.land(Plane.new) }
+airport.capacity.times { airport.land(Plane.new) }
 airport.land(plane)
