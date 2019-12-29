d31b5a6c3a0355985ce8d46f4c38d6d4a47f6ac9
Donald Newholm <newholm1@hotmail.com>
2019-12-29 10:12:48 +0000

    User stories completed, no edge cases yet

--- features/feature.rb
+++ features/feature.rb
@@ -1,10 +1,16 @@
 require './lib/plane.rb'
 plane = Plane.new
 airport = Airport.new
+srand(5)
 airport.land(plane)
+srand(500)
+airport.land(plane)
+srand(5)
 airport.launch(plane)
-airport.capacity.times { airport.land(Plane.new) }
+srand(500)
+airport.launch(plane)
+airport.capacity.times { srand(500); airport.land(Plane.new) }
 airport.land(plane)
 airport_2 = Airport.new(2000)
-2000.times { airport_2.land(Plane.new) }
+2000.times { srand(500); airport_2.land(Plane.new) }
 airport_2.land(plane)
