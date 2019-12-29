91cd2b69c60de1636cebb6604d576049111128b0
Donald Newholm <newholm1@hotmail.com>
2019-12-29 08:50:42 +0000

    Variable capacity added to feature test

--- features/feature.rb
+++ features/feature.rb
@@ -5,3 +5,6 @@ airport.land(plane)
 airport.launch(plane)
 airport.capacity.times { airport.land(Plane.new) }
 airport.land(plane)
+airport_2 = Airport.new(2000)
+2000.times { airport_2.land(Plane.new) }
+airport_2.land(plane)
