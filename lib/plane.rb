a3a3526437d53a41d256096877ee189540c0d53f
Donald Newholm <newholm1@hotmail.com>
2019-12-30 18:24:48 +0000

    Plane will not land in a full hangar

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,6 +3,7 @@ require_relative 'airport'
 class Plane
 
   def land(airport)
+    fail "Hangar full." if airport.full?
     airport.hangar.push(self)
   end
 
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,10 +6,18 @@ describe Plane do
 
     let (:airport) {double (:airport) }
 
-    it 'responds to #land' do
+    it 'lands a plane' do
       allow(airport).to receive(:hangar)
+      allow(airport).to receive(:full?).and_return(false)
       allow(airport.hangar).to receive(:push).and_return(subject)
       expect(subject.land(airport)).to eq(subject)
+    end
+
+    it 'will not land a plane in a full hangar' do
+      allow(airport).to receive(:full?).and_return(true)
+      allow(airport).to receive(:hangar)
+      allow(airport.hangar).to receive(:push).and_return(subject)
+      expect { subject.land(airport) }.to raise_error("Hangar full.")
     end
   end
 end
