d00af3ef7e9b088058893e2ef8569b3ad0097942
Donald Newholm <newholm1@hotmail.com>
2019-12-31 15:11:03 +0000

    Refactor full & stormy tests

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -18,21 +18,17 @@ describe Plane do
     end
 
     it 'will not land a plane in a full hangar' do
-      allow(airport).to receive(:class).and_return(Airport)
-      allow(airport).to receive(:full?).and_return(true)
-      allow(airport).to receive(:hangar)
-      allow(airport.hangar).to receive(:push).and_return(subject)
       subject.flying = true
+      allow(airport).to receive(:class).and_return(Airport)
+      allow(airport).to receive(:full?).and_return(true)      
       expect { subject.land(airport) }.to raise_error("Cannot land - hangar is full.")
     end
 
     it 'will not land a plane if the weather is stormy' do
-      allow(airport).to receive(:full?).and_return(false)
-      allow(airport).to receive(:hangar)
-      allow(airport.hangar).to receive(:push)
-      allow(airport).to receive(:stormy?).and_return(true)
-      allow(airport).to receive(:class).and_return(Airport)
       subject.flying = true
+      allow(airport).to receive(:class).and_return(Airport)
+      allow(airport).to receive(:full?).and_return(false)
+      allow(airport).to receive(:stormy?).and_return(true)
       expect { subject.land(airport) }.to raise_error("Cannot land - weather is inclement.")
     end
 
