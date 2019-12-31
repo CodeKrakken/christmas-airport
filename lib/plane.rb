bf7f70d70f375fc378b4409d869f72d42815ed89
Donald Newholm <newholm1@hotmail.com>
2019-12-31 10:59:29 +0000

    Will not take off if already in flight

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,15 +2,23 @@ require_relative 'airport'
 
 class Plane
 
+  def initialize
+    @flying = false
+  end
+
   def land(airport)
+    fail "Cannot land - not a valid airport." if airport.class != Airport
     fail "Cannot land - hangar is full." if airport.full?
     fail "Cannot land - weather is inclement." if airport.stormy?
+    @flying = false
     airport.hangar.push(self)
   end
 
   def take_off(airport)
+    fail "Cannot take off - already in flight." if @flying
     fail "Cannot take off - weather is inclement." if airport.stormy?
     airport.hangar.delete(self)
+    @flying = true
     "Departure successful."
   end
 
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,10 +4,12 @@ describe Plane do
 
   describe '#land' do
 
-    let (:airport) {double (:airport) }
+    let (:airport) { double (:airport) }
+    let (:crab) { double (:crab) }
 
     it 'lands a plane' do
       allow(airport).to receive(:hangar)
+      allow(airport).to receive(:class).and_return(Airport)
       allow(airport).to receive(:full?).and_return(false)
       allow(airport.hangar).to receive(:push).and_return(subject)
       allow(airport).to receive(:stormy?).and_return(false)
@@ -15,6 +17,7 @@ describe Plane do
     end
 
     it 'will not land a plane in a full hangar' do
+      allow(airport).to receive(:class).and_return(Airport)
       allow(airport).to receive(:full?).and_return(true)
       allow(airport).to receive(:hangar)
       allow(airport.hangar).to receive(:push).and_return(subject)
@@ -26,7 +29,13 @@ describe Plane do
       allow(airport).to receive(:hangar)
       allow(airport.hangar).to receive(:push)
       allow(airport).to receive(:stormy?).and_return(true)
+      allow(airport).to receive(:class).and_return(Airport)
       expect { subject.land(airport) }.to raise_error("Cannot land - weather is inclement.")
+    end
+
+    it 'will not land in a fictitious airport' do
+      
+      expect { subject.land(crab) }.to raise_error("Cannot land - not a valid airport.")
     end
   end
 
@@ -46,6 +55,18 @@ describe Plane do
       allow(airport.hangar).to receive(:delete)
       allow(airport).to receive(:stormy?).and_return(true)
       expect { subject.take_off(airport) }.to raise_error("Cannot take off - weather is inclement.")
+    end
+
+    it 'will not take off if already in flight' do
+      allow(airport).to receive(:class).and_return(Airport)
+      allow(airport).to receive(:full?).and_return(false)
+      allow(airport).to receive(:stormy?).and_return(false)
+      allow(airport).to receive(:hangar)
+      allow(airport.hangar).to receive(:push)
+      allow(airport.hangar).to receive(:delete)
+      subject.land(airport)
+      subject.take_off(airport)
+      expect { subject.take_off(airport) }.to raise_error("Cannot take off - already in flight.")
     end
   end
 end
